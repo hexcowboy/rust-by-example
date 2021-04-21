43012b01853caa7c0cd4c388f191d5a2646809eb
spacecowboy <jacknovotny8@gmail.com>
2021-04-21 10:09:06 -0500

    add comments to hello world project

--- 01-hello_world/hello.rs
+++ 01-hello_world/hello.rs
@@ -1,3 +1,9 @@
+/// This is a documentation comment
 fn main() {
+    // This is an example of a line comment
+    /*
+       This is an example
+       of a multiline comment
+       */
     println!("Hello world");
 }
